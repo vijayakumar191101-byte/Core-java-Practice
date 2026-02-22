492c5252ba9f210a5a3e0f460932cb8587add743
vijayakumar191101-byte <vijayakumar191101@example.com>
2026-02-22 17:09:11 +0530

    remove duplicate characters

--- src/Practice/Remove_Duplicate_Characters.java
+++ src/Practice/Remove_Duplicate_Characters.java
@@ -1,4 +1,14 @@
 package Practice;
 
 public class Remove_Duplicate_Characters {
+    static void main(String[] args) {
+        String s = "programming";
+        String result = "";
+        for (char ch : s.toCharArray()){
+            if (result.indexOf(ch)==-1){
+                result+=ch;
+            }
+        }
+        System.out.println(result);
+    }
 }
